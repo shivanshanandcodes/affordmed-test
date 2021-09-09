d275fec5516d3dd9911ca7252361f26066a673b6
Shivansh Anand <shivanshanandisminame@outlook.com>
2021-09-09 17:18:20 +0530

    Code for q1 : TOyTCPStream receiver and reader

---
+++ q1.py
@@ -0,0 +1,11 @@
+#Author : Shivansh Anand
+#Code for toy TCP stream receiver and reader
+
+
+class ToyTCPStream:
+    def receive(self, chunk, data):
+        pass
+
+
+    def read(self, data):
+        pass
